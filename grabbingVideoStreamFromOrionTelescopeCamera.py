72f9041a930be56390b9fdaaa1b34b734a7517f7
Rob Steele <snobotics981>
2019-10-20 16:45:12 -0700

    Some raspberry pi zero working example python code.

--- grabbingVideoStreamFromOrionTelescopeCamera.py
+++ grabbingVideoStreamFromOrionTelescopeCamera.py
@@ -2,16 +2,10 @@
 #
 # https://gist.github.com/tedmiston/6060034
 #
-# I was looking for examples of grabbing images from a web camera
-# connected to the USB port of my Raspberry Pi Zero.
-# In it's present form the program started grabbing images from my
-# USB camera connected to the Raspberry Pi Zero. I suspect this same
-# code would work equally well on a Raspberry Pi 2/3/or 4.
-
-# This was only grabbing images from the USB camera when I specified
-# VideoCapture(1). When I specified VideoCapture(0) it grabbed from the
-# RaspberryPi Camera.
-
+# This example, as modified by Rob Steele is working to grab a video
+# stream from my Orion Telescope camera.
+#
+# At the default values it was using 100% of the CPU.
 """
 Simply display the contents of the webcam with optional mirroring using OpenCV 
 via the new Pythonic cv2 interface.  Press <esc> to quit.
@@ -20,7 +14,7 @@ via the new Pythonic cv2 interface.  Press <esc> to quit.
 import cv2
 
 def show_webcam(mirror=False):
-    cam0 = cv2.VideoCapture(0)
+    cam0 = cv2.VideoCapture(1)
     while True:
         ret_val0, img0 = cam0.read()
         if mirror: 
