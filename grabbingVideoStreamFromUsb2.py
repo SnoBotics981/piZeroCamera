b835f6f35d5569d62dcdeb21a47289584510fa3e
Rob Steele <snobotics981>
2019-10-20 17:46:46 -0700

    Some more Raspberry Pi example code.

---
+++ grabbingVideoStreamFromUsb2.py
@@ -0,0 +1,38 @@
+# Copied this from the web site:
+#
+# https://gist.github.com/tedmiston/6060034
+#
+# This example, as modified by Rob Steele is working to grab a video
+# stream from my Orion Telescope camera.
+#
+# At the default values it was using 100% of the CPU.
+"""
+Simply display the contents of the webcam with optional mirroring using OpenCV 
+via the new Pythonic cv2 interface.  Press <esc> to quit.
+"""
+
+import cv2
+
+def show_webcam(mirror=False):
+    cam0 = cv2.VideoCapture(2)
+    while True:
+        ret_val0, img0 = cam0.read()
+        if mirror: 
+            img0 = cv2.flip(img0, 1)
+        cv2.imshow('my webcam 0', img0)
+       	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+
+	if key == ord("q"):
+		break
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    main()
+
